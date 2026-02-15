077938c78a5c1241ce8dc3c819755a8f7cb66051
IMxDanny <desaleom777@gmail.com>
2026-02-15 12:32:31 +0530

    modal changed

--- utils/GeminiAIModal.js
+++ utils/GeminiAIModal.js
@@ -12,7 +12,7 @@ function initializeChat() {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.5-pro",
   });
 
   const generationConfig = {
